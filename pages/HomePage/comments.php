44287043ccafbe35d72cefa8d41d3f16348cdd4f
Ravidu Shehan <ravidu.2017486@iit.ac.lk>
2021-01-16 13:28:49 +0530

    added changes

--- pages/HomePage/comments.php
+++ pages/HomePage/comments.php
@@ -1,6 +1,6 @@
 <?php
 
-    $host = "localhost";
+$host = "localhost";
     $dbUsername = "root";
     $dbPassword = "";
     $dbname = "pawcare";
@@ -8,56 +8,13 @@
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
 // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-//   }
-//   echo "Connected successfully";
-
-// $parent_id = "";
-// $firstname = $_POST['firstname'];
-// $lastname = $_POST['lastname'];
-// $comment = $_POST['comment'];
-// $date = date('Y-m-d H:i:s');
-
-// $sql = "INSERT INTO comments(parent_id,firstname,lastname,comment,comment_date) VALUES ('" . $parent_id . "','" . $firstname . "','" . $lastname . "','" . $comment . "','" . $date . "')";
-// $result = mysqli_query($conn, $sql);
-
-// if (! $result) {
-//     $result = mysqli_error($conn);
-// }
-// echo $result;
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$id = isset($_POST['id']) ? $_POST['id'] : "";
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$comment = $_POST['comment'];
-$date = date('Y-m-d H:i:s');
-
-  if (!empty($firstname) || !empty($lastname) || !empty($comment) || !empty($date)){
-      // $sql = "INSERT INTO user (username, email, mobile, password)
-      //         SELECT * FROM (SELECT '$username' AS username, '$email' AS email, '$mobile' AS mobile, '$password' AS password)
-      //         WHERE NOT EXISTS (
-      //         SELECT name FROM user WHERE email = $email
-      //         ) LIMIT 1";
-
-      $sql = "INSERT INTO comments (parent_id,firstname, lastname, comment, comment_date) VALUES('$id','$firstname', '$lastname', '$comment', '$date')";
-
-  if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully";
-  } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
+    die("Connection failed: " . $conn->connect_error);
   }
+  echo "Connected successfully";
 
-      
-  }else {
-      echo"All fields are required";
-      die();  
-  }
-  $conn->close();
+
+
 
 
 ?>
--- pawcare.sql
+++ pawcare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2021 at 02:32 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Generation Time: Jan 10, 2021 at 11:01 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,31 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `pawcare`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `comment` text NOT NULL,
-  `comment_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `parent_id`, `firstname`, `lastname`, `comment`, `comment_date`) VALUES
-(12, 0, 'asdasd', 'asdas asd', '                asdasd sda', '2021-01-15 13:37:15'),
-(13, 0, 'aaaa', 'bbbbb', 'aaaa bbbbbcccc', '2021-01-15 13:40:09'),
-(14, 0, 'ccc', 'cccc', '              cccc ddd  ', '2021-01-15 13:40:40'),
-(15, 0, 'sdfdfdf', 'dsfsdf', '                sdfsdfsdfd', '2021-01-15 14:01:20');
 
 -- --------------------------------------------------------
 
@@ -65,19 +40,6 @@ CREATE TABLE `sitter` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_like_unlike`
---
-
-CREATE TABLE `tbl_like_unlike` (
-  `id` int(11) NOT NULL,
-  `comment_id` int(11) NOT NULL,
-  `like_unlike` int(2) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
@@ -86,41 +48,28 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `mobile` int(10) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `favoutites` varchar(300) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `email`, `mobile`, `password`, `favoutites`) VALUES
-(1, 'senuri', 'senuri@gmail.com', 711234123, 'senuri123', 'Anne'),
-(2, 'ravidu', 'ravidu@gmail.com', 711234123, 'ravidu123', 'Anne'),
-(3, 'chanuka', 'chanuka@gmail.com', 711234123, 'chanuka123', 'Anne'),
-(4, 'dilki', 'sasi@gmail.com', 711234123, 'sasi123', 'Anne');
+INSERT INTO `user` (`user_id`, `username`, `email`, `mobile`, `password`) VALUES
+(1, 'senuri', 'senuri@gmail.com', 711234123, 'senuri123'),
+(2, 'ravidu', 'ravidu@gmail.com', 711234123, 'ravidu123'),
+(3, 'chanuka', 'chanuka@gmail.com', 711234123, 'chanuka123'),
+(4, 'dilki', 'sasi@gmail.com', 711234123, 'sasi123');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `sitter`
 --
 ALTER TABLE `sitter`
   ADD PRIMARY KEY (`sitter_id`);
-
---
--- Indexes for table `tbl_like_unlike`
---
-ALTER TABLE `tbl_like_unlike`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -131,12 +80,6 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `sitter`
